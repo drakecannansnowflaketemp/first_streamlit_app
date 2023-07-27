f0f7fe9c2d1e883b3f7c222b8113db119f97eecb
drakecannansnowflaketemp <140553315+drakecannansnowflaketemp@users.noreply.github.com>
2023-07-26 21:05:22 -0700

    Update streamlit_app.py
    Modify Fruityvice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,18 @@ streamlit.dataframe(fruits_to_show)
 
 #Fruityvice section
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get informatin.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except:
+  streamlit.error()
 
-# Convert JSON response to DataFrame 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display DataFrame
-streamlit.dataframe(fruityvice_normalized)
 
 # Don't run anything past this while we troubleshoot
 streamlit.stop()
